f4d1efe051f2b839ba98af0a54caee6e0f668cac
EvilDunk <45837337+EvilDunk@users.noreply.github.com>
2020-01-06 20:41:04 -0800

    Add files via upload
    Builds a simple pyramid as you might see on a Mario Bros level using #'s

---
+++ marioless.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{   
+    // Make variables
+    int height;
+    int row;
+    int spaces;
+    int hashes;
+    
+    // Get the number
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8); // Is the number 1-8?
+    // If not 1-8, prompt user for height again
+   
+    // Count the rows
+    for (row = 1; row <= height; row++)
+    { 
+        // Print spaces
+        for (spaces = height - row; spaces > 0; spaces--)
+        {
+            printf(" ");
+        }
+        
+        // Print hashes
+        for (hashes = 0; hashes < row; hashes++)
+        {
+            printf("#");
+        }
+        
+        // Start new line
+        printf("\n");
+        
+    }
+}
